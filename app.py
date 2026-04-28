9f92e72ffe79ed160c82eee0f6134551b7dc9a87
PranayKrGupta <techyaar98353@gmail.com>
2026-04-28 12:26:23 +0530

    changed PORT configuration

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 import heapq
+import os
 
 app = Flask(__name__)
 
@@ -114,4 +115,5 @@ def route_optimized():
     })
 
 if __name__ == "__main__":
-    app.run(debug=False, host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
